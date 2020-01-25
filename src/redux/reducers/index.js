7d940277f3525eb9638246f7904a3de7e6a4f5ad
igor <embiggenerd@gmail.com>
2020-01-25 15:39:55 -0500

    redux fully integrated, actions and state operating correctly

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,21 @@
-export const rootReducer = (state = { locked: false, closed: false }, { type, payload }) => {
+import { TOGGLE_CLOSED, TOGGLE_LOCKED } from '../constants'
+
+export const rootReducer = (
+    state = { locked: false, closed: false },
+    { type, payload }
+) => {
+
     switch (type) {
+        case TOGGLE_CLOSED:
+            return {
+                ...state,
+                closed: !state.closed
+            }
+        case TOGGLE_LOCKED:
+            return {
+                ...state,
+                locked: !state.locked
+            }
         default:
             return state
     }
