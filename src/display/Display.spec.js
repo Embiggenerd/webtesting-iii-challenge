d22a9aa1e07ebb9fe3f85f4135da262066e50118
igor <embiggenerd@gmail.com>
2020-01-25 16:48:24 -0500

    display prop tests passing

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -6,17 +6,29 @@ import Display from './Display'
 
 const defaultProps = { locked: false, closed: false }
 const closedUnlockedProps = { locked: false, closed: true }
+const closedLockedProps = { locked: true, closed: true }
 
-test('can render with redux with defaults', () => {
+test('can render with default props with green-led class', () => {
     const { getByTestId } = render(<Display {...defaultProps} />)
-    // fireEvent.click(getByText('+'))
+
+    expect(getByTestId('display-closed')).toHaveClass('green-led')
+    expect(getByTestId('display-locked')).toHaveClass('green-led')
     expect(getByTestId('display-closed')).toHaveTextContent('Open')
     expect(getByTestId('display-locked')).toHaveTextContent('Unlocked')
 })
 
-test('can render with redux with defaults', () => {
+test('displays closed, unlocked correctly', () => {
     const { getByTestId } = render(<Display {...closedUnlockedProps} />)
-    // fireEvent.click(getByText('+'))
-    expect(getByTestId('display-closed')).toHaveTextContent('Open')
+
+    expect(getByTestId('display-closed')).toHaveTextContent('Closed')
     expect(getByTestId('display-locked')).toHaveTextContent('Unlocked')
+})
+
+test('can render with closed locked defaults with red-led class', () => {
+    const { getByTestId } = render(<Display {...closedLockedProps} />)
+
+    expect(getByTestId('display-closed')).toHaveClass('red-led')
+    expect(getByTestId('display-locked')).toHaveClass('red-led')
+    expect(getByTestId('display-closed')).toHaveTextContent('Closed')
+    expect(getByTestId('display-locked')).toHaveTextContent('Locked')
 })
